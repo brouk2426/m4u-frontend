cfb9dc85bfcb60926de03668056695d9084533a4
brouk2426 <48667021+brouk2426@users.noreply.github.com>
2021-11-25 00:06:28 +0900

    style: Set global styles (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from "next/app";
+import global from "../styles/global";
+import GlobalLayout from "./components/base/GlobalLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </>
+  );
 }
 
 export default MyApp;
